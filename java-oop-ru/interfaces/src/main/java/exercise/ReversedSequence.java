e79cdefa4bba7b9d441d65e927a0708917b2f21b
@hexlet/cli <support@hexlet.io>
2024-03-15 20:10:27 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -1,5 +1,30 @@
 package exercise;
 
 // BEGIN
+public class ReversedSequence implements CharSequence {
+    private String reversedText;
 
+    public ReversedSequence(String text) {
+        this.reversedText = new StringBuilder(text).reverse().toString();
+    }
+
+    @Override
+    public int length() {
+        return reversedText.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return reversedText.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return reversedText.subSequence(start, end);
+    }
+
+    public String toString() {
+        return reversedText;
+    }
+}
 // END
