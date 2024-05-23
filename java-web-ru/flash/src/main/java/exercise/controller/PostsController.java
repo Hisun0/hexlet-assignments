bb5d9ddf6b25ea630d33c978b7caed2f143b093b
@hexlet/cli <support@hexlet.io>
2024-05-23 12:04:01 +0300

    submit java-web-ru/flash

---
+++ java-web-ru/flash/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,57 @@
+package exercise.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.dto.posts.BuildPostPage;
+import exercise.util.NamedRoutes;
+import io.javalin.http.Context;
+import io.javalin.validation.ValidationException;
+import io.javalin.http.NotFoundResponse;
+
+public class PostsController {
+
+    public static void build(Context ctx) {
+        var page = new BuildPostPage();
+        ctx.render("posts/build.jte", model("page", page));
+    }
+
+    // BEGIN
+    public static void index(Context ctx) {
+        var posts = PostRepository.getEntities();
+
+        var page = new PostsPage(posts);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        ctx.render("posts/index.jte", model("page", page));
+    }
+
+    public static void create(Context ctx) {
+        try {
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(p -> p.length() >= 2, "Название слишком короткое!").get();
+            var body = ctx.formParam("body");
+            var post = new Post(name, body);
+
+            PostRepository.save(post);
+            ctx.sessionAttribute("flash", "Пост был успешно создан!");
+            ctx.redirect(NamedRoutes.postsPath());
+        } catch (ValidationException err) {
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
+            var page = new BuildPostPage(name, body, err.getErrors());
+            ctx.render("posts/build.jte", model("page", page));
+        }
+    }
+    // END
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+            .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", model("page", page));
+    }
+}
